fecfbb0b900a0021ce17303cd6120bca321b83ae
Ashley Miller <asm2fe@virginia.edu>
2019-04-10 20:04:31 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,19 @@
 # Your code goes here!
+require 'pry'
 class Anagram
+  attr_accessor :letters
+  def initialize(word)
+    @letters = word.split("")
+    @letters.sort!
+  end
+
+  def match(text)
+    letarray = []
+    m = []
+    text.each {|x| letarray << x.split("")}
+    #binding.pry
+    m << letarray.select {|y| y.sort==@letters}
+    m
+    binding.pry
+  end
 end
