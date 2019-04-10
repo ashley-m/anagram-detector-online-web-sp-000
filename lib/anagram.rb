770ccf7735f6e78c8cf0bcbcdb39c218664cd30e
Ashley Miller <asm2fe@virginia.edu>
2019-04-10 20:14:57 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,9 +13,9 @@ class Anagram
     text.each {|x| letarray << x.split("")}
     #binding.pry
     m << letarray.select {|y| y.sort==@letters}
-    binding.pry
     m.flatten!(1) if m.any? {|z| z.is_a?(Array)}
     m.each {|f| f.join if f.is_a?(Array)}
+    binding.pry
     m
   end
 end
